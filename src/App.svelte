dc592cf868f30b0a6357b3e343e5ff1d77b615fa
Michael van Gorkum <michaelvangorkum@msn.com>
2019-09-29 16:12:20 +0200

    Added HireEmployee component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,14 @@
 <script>
 	let name = 'world';
+	import Hire from './HireEmployee.svelte';
 </script>
 
 <style>
 	h1 {
-		color: purple;
+		color: red;
 	}
 </style>
 
 <h1>Hello {name}!</h1>
+<Hire/>
+
---
+++ src/HireEmployee.svelte
@@ -0,0 +1,22 @@
+<script>
+    const employeeCount = document.getElementById('employees__count');
+    const resource = 1234;
+    function handleEmployee() {
+        employeeCount.innerHTML = Math.round(resource);
+    }
+
+</script>
+
+<button on:click={handleEmployee}>
+    Hire employee
+</button>
+
+
+<div id="employees__count" class="employees__count">
+    0
+</div>
+<!-- 
+ resource += upgrade;
+ display.innerHTML = Math.round(resource);
+ setTimeout(startCount, 15);
+ startcount.style.display = 'none'; -->
